7c27e52556bb07686bc2eda0c35dd864b365f1c1
Capi7977 <129705834+Capi7977@users.noreply.github.com>
2024-12-08 02:29:10 -0700

    I have created a popup telling the user a message using string along with the code of the locker using psudocode

---
+++ javascript.js
@@ -0,0 +1,11 @@
+// The Fist thing I have done is created variable making sure the pseudocode matching the correct number to the locker
+let num1 = 5 + 5;
+let num2 = 20 * 2;
+let num3 = 40 - 1;
+
+// I have created a varible with strings
+const reason = " because you have been chosen to open an important vault."
+const vaultcombination = " Here is the secret combination: "
+
+// I have made a popup using alert, telling the user a message using strings, and  with the vault combination using the numbers
+alert("You have received this message " + reason + vaultcombination + " " + num1 + " -" + " " + num2 + " -" + " " + num3)
